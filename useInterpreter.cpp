059e729c1e57cefe697c3b4dcb669621bf3d6a59
T-Rzeznik <trzeznik29@gamil.com>
2024-03-25 15:27:17 -0400

    create repo

---
+++ useInterpreter.cpp
@@ -0,0 +1,13 @@
+//**************************  useInterpreter.cpp   ***********************
+
+#include "interpreter.h"
+
+int main() {
+    Statement statement;
+    cout << "The program processes statements of the following format:\n"
+         << "\t<id> = <expr>$\n\tprint <id>\n\tstatus\n\tend\n\n"; //changing terminator ot $ instead ;
+    while (true)                  // This infinite loop is broken by exit(1)
+        statement.getStatement(); // in getStatement() or upon finding 
+    return 0;                     // an error.
+}
+
